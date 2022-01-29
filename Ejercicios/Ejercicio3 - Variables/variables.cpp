d435febec529a0fc960aca7b1f871bf349e9dbab
Josafat1311 <josafat1311@gmail.com>
2022-01-28 20:16:24 -0600

    agregando ejercicio 4 y 5

---
+++ Ejercicios/Ejercicio3 - Variables/variables.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+using namespace std;
+
+int main(int argc, char const *argv[])
+{
+    int numero_entero;
+    numero_entero = 15;
+
+    cout << "Entero: " << numero_entero << endl;
+
+    numero_entero = 18;
+
+    cout << "Entero modificado: " << numero_entero;
+
+    return 0;
+}
---
+++ Ejercicios/Ejercicio4 - Imprimir Valores de Todas las Variables/todas-las-variables.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+
+using namespace std;
+
+int main(int argc, char const *argv[])
+{
+    bool valor_boolean;
+    valor_boolean = true;
+
+    int valor_entero = 15;
+    double valor_double = 20.99;
+    string valor_string = "Hola como estan";
+    char valor_char = 'B';
+
+    cout << "Valor Boolean: " << valor_boolean << endl;
+    cout << "Valor Entero: " << valor_entero << endl;
+    cout << "Valor Double: " << valor_double << endl;
+    cout << "Valor String: " << valor_string << endl;
+    cout << "Valor Char: " << valor_char << endl;
+    return 0;
+}
